e7058ee170057ab95267fbc53237b7f25fb6051d
jhalford26 <63118766+jhalford26@users.noreply.github.com>
2020-05-24 21:21:02 -0500

    tienda2.5

--- Proyecto/Assets/Shop.cs
+++ Proyecto/Assets/Shop.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Globalization;
 public class Shop : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] objetos=new GameObject[16];
+    public Dictionary<int, GameObject> diccionario = new Dictionary<int, GameObject>();
     void Start()
     {
+        InsertandoALDiccionario();
+        
+
 
     }
 
@@ -114,4 +119,102 @@ public class Shop : MonoBehaviour
             
         }
     }
-}
+
+    public void tipo(int tipo)
+    {
+
+        switch (tipo)
+        {
+
+            case 0:
+                foreach (int ind in diccionario.Keys)
+                {
+                    string nombre = "" + ind;
+                    char indexe = char.Parse(nombre.Substring(0, 1));
+
+                    
+
+                        diccionario[ind].SetActive(true);
+                   
+
+                }
+
+                break;
+
+
+            case 1:
+                foreach(int ind in diccionario.Keys)
+                {
+                    string nombre= ""+ind;
+                    char indexe = char.Parse(nombre.Substring(0, 1));
+                    
+                    if (indexe == '1')
+                    {
+                        
+                        diccionario[ind].SetActive(true);
+                    }
+                    else
+                    {
+                        diccionario[ind].SetActive(false);
+                    }
+                    
+
+                }
+                
+                break;
+
+            case 2:
+                foreach (int ind in diccionario.Keys)
+                {
+                    string nombre = "" + ind;
+                    char indexe = char.Parse(nombre.Substring(0, 1));
+                    if (indexe == '2')
+                    {
+                        diccionario[ind].SetActive(true);
+                    }
+                    else
+                    {
+                        diccionario[ind].SetActive(false);
+                    }
+
+
+                }
+                break;
+
+
+            case 3:
+                foreach (int ind in diccionario.Keys)
+                {
+                    string nombre = "" + ind;
+                    char indexe = char.Parse(nombre.Substring(0, 1));
+                    if (indexe == '3')
+                    {
+                        diccionario[ind].SetActive(true);
+                    }
+                    else
+                    {
+                        diccionario[ind].SetActive(false);
+                    }
+
+
+                }
+                break;
+        }
+
+
+    }
+
+    public void InsertandoALDiccionario()
+    {
+        int count =0;
+        foreach(GameObject obj in objetos)
+        {
+            count++;
+            string tem = obj.name + count;
+            int key = 0;
+            int.TryParse(tem, out key);
+            diccionario.Add(key, obj);
+        }
+
+    }
+ }
