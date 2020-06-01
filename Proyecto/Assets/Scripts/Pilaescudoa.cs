c30718d0a2aa9b4069db1dd4950280a15595c3b8
xSalcedo <martin.alejandro23@gmail.com>
2020-06-01 17:06:23 -0500

    Pila y actualizacion arbol
    a

--- Proyecto/Assets/Scripts/Pilaescudoa.cs
+++ Proyecto/Assets/Scripts/Pilaescudoa.cs
@@ -1,42 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Pilaescudoa : MonoBehaviour
+namespace AAAAAA
 {
-    public Pila lul;
-    float d = 0;
-    int id = 1;
-    public GameObject Tpp;
-    Dictionary<int, GameObject> aI = new Dictionary<int, GameObject>();
-    Pila Estado = new Pila(3);
-    // Start is called before the first frame update
-    void Start()
+    public class Pilaescudoa : MonoBehaviour
     {
+        public Pila lul;
+        public ListaMisionesP xd;
+        float d = 0;
+        int id = 1;
+        public GameObject Tpp;
+        Dictionary<int, GameObject> aI = new Dictionary<int, GameObject>();
+        //Pila Estado = new Pila(3);
+        ListaMisionesP Estadoa = new ListaMisionesP();
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Gemas"))
-        {
-            other.transform.position = Tpp.transform.position + new Vector3(d, 0.0f, 0.0f);
-            d += 10;
-            aI.Add(id, other.gameObject);
-            Estado.push(id);
-            id++;
-            print(id);
         }
-        if (other.gameObject.CompareTag("F"))
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            Destroy(other.gameObject);
-            Destroy(aI[Estado.pop()]);
-            d -= 10;
-            other.transform.position = Tpp.transform.position + new Vector3(d, 0.0f, 0.0f);
+            if (other.gameObject.CompareTag("Gemas"))
+            {
+                other.transform.position = Tpp.transform.position + new Vector3(d, 0.0f, 0.0f);
+                d += 10;
+                aI.Add(id, other.gameObject);
+                //Estado.push(id);
+                Estadoa.IngresarCabeza(id);
+                id++;
+                print(id);
+            }
+            if (other.gameObject.CompareTag("F"))
+            {
+                if (!Estadoa.empty())
+                {
+                    Destroy(other.gameObject);
+                    Destroy(aI[Estadoa.RInicio()]);
+                    d -= 10;
+                    other.transform.position = Tpp.transform.position + new Vector3(d, 0.0f, 0.0f);
+                }
+                else
+                {
+                    print("XD");
+                    Destroy(other.gameObject);
+                }
+            }
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
