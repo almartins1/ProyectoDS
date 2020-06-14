8bea385f7d3262b1f98c9bf1f66050a65fbf61b2
jhalford26 <63118766+jhalford26@users.noreply.github.com>
2020-06-14 16:34:45 -0500

    car

---
+++ Proyecto/Assets/Scripts/Mision1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mision1 : MonoBehaviour
+{
+    public GameObject cartel;
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            
+            
+            
+            Destroy(cartel);
+
+        }
+    }
+}
