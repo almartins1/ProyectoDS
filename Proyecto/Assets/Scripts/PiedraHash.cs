7fa567530cdd21326d88acbaab9e8ea23ed321f5
xSalcedo <martin.alejandro23@gmail.com>
2020-07-04 15:36:51 -0500

    Update PiedraHash.cs

--- Proyecto/Assets/Scripts/PiedraHash.cs
+++ Proyecto/Assets/Scripts/PiedraHash.cs
@@ -6,10 +6,11 @@ public class PiedraHash : MonoBehaviour
 {
     public HashC XD;
     HashC l = new HashC(3);
+    int c = 0;
     // Start is called before the first frame update
     void Start()
     {
-        l.InsertarObj(2);
+        /*l.InsertarObj(2);
         l.InsertarObj(5);
         l.InsertarObj(8);
         l.InsertarObj(4);
@@ -23,7 +24,16 @@ public class PiedraHash : MonoBehaviour
         l.InsertarObj(2);
         l.imp(2);
         l.imp(1);
-        l.imp(3);
+        l.imp(3);*/
+        float timeNow1 = Time.realtimeSinceStartup;
+        print(timeNow1);
+        while (c < 10000){           
+            l.InsertarObj(c);
+            c++;
+            
+        }
+        float timeNow2 = Time.realtimeSinceStartup;
+        print(timeNow2);
 
     }
 
